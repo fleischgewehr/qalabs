eb8c02372f3e6a542ffc939e5c72abf81f0cedec
sustinianvanxh <sustinianvanxh@gmail.com>
2021-03-08 01:39:42 +0200

    New tests

--- lab2/manager.py
+++ lab2/manager.py
@@ -2,6 +2,12 @@ from exceptions import BaseError
 from configparser import ConfigParser
 from server import Server, Request, Response
 
+class ConfigParserFactory():
+
+	def create_parser(self, conf_path:str) -> None:
+		conf = ConfigParser()
+		conf.read(conf_path)
+		return conf
 
 class ExceptionManager:
 
@@ -10,14 +16,25 @@ class ExceptionManager:
 	_config: ConfigParser
 	_server: Server
 
-	def __init__(self, server: Server, config: str = 'manager.ini') -> None:
+	def __init__(
+				self, server: Server, 
+				config: ConfigParser = ConfigParserFactory().create_parser('manager.ini')
+				) -> None:
+
 		self._server = server
-		self._config = ConfigParser()
-		self._config.read(config)
+		self._config = config
 
 		self.critical_exc_counter = 0
 		self.regular_exc_counter = 0
 		self.server_exc_not_handled = 0
+
+	@property
+	def server(self) -> None:
+		return self._server
+
+	@server.getter
+	def server(self, value:ConfigParser) -> None:
+		self._server = value
 
 	def is_critical(self, exc: BaseError) -> bool:
 		exec_name = exc.__class__.__name__
@@ -28,11 +45,12 @@ class ExceptionManager:
 		response = self._server.handle_request(request).status.value
 		return response
 
+	def server_exc_inc(self) -> None:
+		self.server_exc_not_handled += 1;
 
 	def check(self, exc: Exception) -> None:
-		
 		if not self.server_handle_exc(exc):
-			self.server_exc_not_handled += 1
+			self.server_exc_inc()
 
 		if not issubclass(type(exc), BaseError):
 			return
--- lab2/test_manager.py
+++ lab2/test_manager.py
@@ -1,21 +1,38 @@
-from manager import ExceptionManager
-from exceptions import CatIsNotFedError, KeyboardSpilledTeaError
+from manager import ExceptionManager, ConfigParserFactory
+from exceptions import CatIsNotFedError, KeyboardSpilledTeaError, SomeExtraordinaryError
 from server import Server
-
+from configparser import ConfigParser
 import pytest
 
 
+class FakeConfigParser(ConfigParser):
+	is_critical: bool
+
+	def getboolean(self, *_):
+		return self.is_critical
+
+
+class FakeConfigCreator(ConfigParserFactory):
+	def create_parser(self, is_critical):
+		parser = FakeConfigParser()
+		parser.is_critical = is_critical
+		return parser
+
+
 @pytest.mark.parametrize(
-	'errors, critical_counter_expected, regular_counter_expected',
+	'errors, critical_status, critical_counter_expected, regular_counter_expected',
 	[
-		((), 0, 0),
-		((CatIsNotFedError, CatIsNotFedError, CatIsNotFedError, KeyboardSpilledTeaError), 3, 1),
-		((KeyboardSpilledTeaError, CatIsNotFedError, KeyboardSpilledTeaError), 1, 2),
+		((CatIsNotFedError, KeyboardSpilledTeaError),(True,False), 1, 1),
+		((CatIsNotFedError, CatIsNotFedError),(True,True), 2, 0),
+		((KeyboardSpilledTeaError, KeyboardSpilledTeaError),(False,False), 0, 2),
 	],
 )
-def test_counter(errors, critical_counter_expected, regular_counter_expected):
-	em = ExceptionManager(Server())
-	for error in errors:
+def test_constructor(errors, critical_status, critical_counter_expected, regular_counter_expected):
+	config = FakeConfigParser() 
+	em = ExceptionManager(Server(), config)
+	
+	for error, is_critical, in zip(errors, critical_status):
+		config.is_critical = is_critical
 		em.check(error())
 
 	assert em.critical_exc_counter == critical_counter_expected
@@ -23,9 +40,78 @@ def test_counter(errors, critical_counter_expected, regular_counter_expected):
 
 
 @pytest.mark.parametrize(
-	'error, critical',
-	[(CatIsNotFedError, True), (KeyboardSpilledTeaError, False)],
+	'errors, critical_status, critical_counter_expected, regular_counter_expected',
+	[
+		((CatIsNotFedError, KeyboardSpilledTeaError),(True,False), 1, 1),
+		((CatIsNotFedError, CatIsNotFedError),(True,True), 2, 0),
+		((KeyboardSpilledTeaError, KeyboardSpilledTeaError),(False,False), 0, 2),
+	],
 )
-def test_is_critical(error, critical):
+def test_property(errors, critical_status, critical_counter_expected, regular_counter_expected):
+	config = FakeConfigParser() 
 	em = ExceptionManager(Server())
-	assert em.is_critical(error()) is critical
+	
+	for error, is_critical, in zip(errors, critical_status):
+		config.is_critical = is_critical
+		em.config = config
+		em.check(error())
+
+	assert em.critical_exc_counter == critical_counter_expected
+	assert em.regular_exc_counter == regular_counter_expected
+
+
+@pytest.mark.parametrize(
+	'errors, critical_status, critical_counter_expected, regular_counter_expected',
+	[
+		((CatIsNotFedError, KeyboardSpilledTeaError),(True,False), 1, 1),
+		((CatIsNotFedError, CatIsNotFedError),(True,True), 2, 0),
+		((KeyboardSpilledTeaError, KeyboardSpilledTeaError),(False,False), 0, 2),
+	],
+)
+def test_factory(errors, critical_status, critical_counter_expected, regular_counter_expected):
+	config_creator = FakeConfigCreator().create_parser
+	em = ExceptionManager(Server())
+	
+	for error, is_critical, in zip(errors, critical_status):
+		config = config_creator(error)
+		em.config = config
+		em.check(error())
+
+	assert em.critical_exc_counter == critical_counter_expected
+	assert em.regular_exc_counter == regular_counter_expected
+
+
+
+# @pytest.mark.parametrize(
+# 	'error, critical',
+# 	[(CatIsNotFedError, True), (KeyboardSpilledTeaError, False)],
+# )
+# def test_property(error, critical):
+# 	em = ExceptionManager(Server())
+# 	error.is_critical = critical
+# 	em.is_critical = lambda error: error.is_critical
+
+# 	assert em.is_critical(error()) is critical
+
+
+# @pytest.mark.parametrize(
+# 	'error, handled, crit_counter_exp, reg_counter_exp, server_counter_exp',
+# 	[
+# 	(CatIsNotFedError, True, 1, 0, 0), 
+# 	#(KeyboardSpilledTeaError, False, 0, 1, 0),
+# 	#(SomeExtraordinaryError, False, 0, 0, 1)
+# 	],
+# )
+# def test_server_exc_counter(error, handled, crit_counter_exp, reg_counter_exp, server_counter_exp):
+# 	em = ExceptionManager(Server())
+
+# 	em.server_handle_exc = lambda _: handled
+
+# 	em.check(error)
+# 	print(error)
+# 	print(em.critical_exc_counter)
+# 	print(crit_counter_exp)
+
+# 	assert em.critical_exc_counter == crit_counter_exp
+# 	#assert em.regular_exc_counter == reg_counter_exp
+# 	#assert em.server_exc_not_handled == server_counter_exp
